a5b811214636022b6ca3bcf3558ac2a6b268d25c
loronghitam <sutio.mudiarno@gmail.com>
2022-12-03 16:55:27 +0700

    update Portfolio.jsx

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,17 +1,38 @@
 import  React from "react"
+import {Visibility} from "@mui/icons-material";
 
+const allCategory = ['all', ...new Set(portfolio.map((item) => item.category))]
 export  const Portfolio = () => {
+
     const [list, setList] = userState(portfolio)
+    const [category, setCategory] = userState(allCategory)
+    const  filterItems  = (category) => {
+        const newItems = portfolio.filter((item) => item.category === category)
+        setList(newItems)
+        if(category==="all"){
+            setList(portfolio)
+        }
+    }
   return (<>
   <article>
       <div className='container'>
-      <div className='catButton'>text</div>
+          <Heading title='Porfolio'/>
+        <div className='catButton'>
+            {category.map((category) => (
+                <button className="primaryBtn" onClick={() => filterItems(category)}>{}</button>
+            ))}
+        </div>
       </div>
       <div className='content grid-3'>
           {List.map((items) =>(
               <div className='box'>
                   <div className='img'>
                       <img src="{items.cover}" alt=""/>
+                  </div>
+                  <div className="overlay">
+                      <h3>{items.title}</h3>
+                      <span>{items.name}</span>
+                      <Visibility />
                   </div>
               </div>
           ))}
