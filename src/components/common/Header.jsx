c05f7b070628ae859495ca313ebd70a1052d9b7e
uwllphile <uwinotuwl@gmail.com>
2022-12-03 10:52:37 +0700

    ubah header,pages jx, app js

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link, NavLink } from "react-router-dom"
+
+export const Header = () => {
+    return (
+        <>
+            <header>
+                <div className='container flexsb'>
+                    <div className='logo'>
+                        <img src={logo} alt=''/>
+                    </div>
+                    <div className='nav'>
+                        {navLink.map((links, i) => (
+                            <Link to={links.url} key={i}>
+                                {links.text}
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,3 +1,23 @@
-import React from 'react'
+import React from "react"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import { Header } from "../common/Header"
 
-export const Pages = ()
+export const Pages = () => {
+    return (
+        <> 
+        <Router>
+            <Header/> 
+            <Switch>
+                <Route path='/about'>
+                    <about/>
+                </Route>
+            </Switch>
+        </Router>
+        </>
+    )
+}
