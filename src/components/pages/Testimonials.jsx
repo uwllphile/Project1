ff3308ec69c18a79f28785efde5a02a8305916b0
loronghitam <sutio.mudiarno@gmail.com>
2022-12-04 00:55:02 +0700

    update Testimonials.jsx

---
+++ src/components/pages/Testimonials.jsx
@@ -0,0 +1,30 @@
+import{FromQuote} from "@mui/icons-material"
+import React from "react"
+import {testimonials} from "../data/dummydata"
+import  "slick-carousel/slick/slick.css"
+import  "slick-carousel/slick/slick-theme.css"
+import slider from "react-slick"
+
+export  const Testimonials = () => {
+    return (
+        <>
+        <div className="testimonial hero">
+            <div className="container">
+                {testimonials.map((val) =>(
+                    <div className="box">
+                        <i>
+                            <FormatQuote />
+                        </i>
+                            <p>{val.text}</p>
+                            <div className="img">
+                                <img src={val.imagaes} alt=""/>
+                            </div>
+                            <h3>{val.name}</h3>
+                            <label>{val.post}</label>
+                    </div>
+                ))}
+            </div>
+        </div>
+        </>
+    )
+}
