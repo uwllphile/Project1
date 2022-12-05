e9d08ae0ab23a26a7231f3537d27325a1a10cd6b
uwllphile <uwinotuwl@gmail.com>
2022-12-05 10:52:00 +0700

    add about

--- src/components/data/dummydata.js
+++ src/components/data/dummydata.js
@@ -33,7 +33,7 @@ export const navlink = [
 export const home = [
     {
         text: "HELLO I'M",
-        name: "ALAMIN MUSA",
+        name: "UWLPHILE",
         post: "WEB DESIGNER",
         design: "UI / UX DESIGNER",
         desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
